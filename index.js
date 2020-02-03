9d200b98bc3ed709bbf840360a597ef3ad8d64bb
qStivi <stephanglaue@yahoo.de>
2020-02-03 16:36:22 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(8080, function(){
+    console.log('listening on *:3000');
+});
