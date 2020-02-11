345ba8fce77919dfc7cf148b4863a8db5eb88b72
qStivi <stephanglaue@yahoo.de>
2020-02-11 07:30:48 +0100

    tried room creation

--- index.js
+++ index.js
@@ -8,15 +8,27 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
     console.log('a user connected');
-    socket.on('disconnect', function(){
+
+    socket.on('create room', function (room) {
+        console.log('creating room... ' + room);
+        socket.join(room);
+        console.log('room ' + room + ' created');
+    });
+
+
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
-    socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
+    socket.on('chat message', function (msg) {
+        console.log(socket.rooms[0] + ': message: ' + msg);
+        io.to('lol').emit('chat message', msg);
     });
 });
 
-http.listen(8080, function(){
+http.listen(3000, function () {
     console.log('listening on *:3000');
 });
+
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low)
+}
