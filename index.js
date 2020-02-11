b52bb86494f35ca9895f3781e9315bdf955f306a
stivi313131 <stephanglaue@yahoo.de>
2020-02-11 09:26:07 +0100

    Chat and rooms now functional

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+var roomName;
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+    socket.leaveAll();
     console.log('a user connected');
 
     socket.on('create room', function (room) {
         console.log('creating room... ' + room);
         socket.join(room);
         console.log('room ' + room + ' created');
+        roomName = room;
     });
 
 
     socket.on('disconnect', function () {
+        roomName = 'üpoiuztrewqasdfghjklö.,mnbvcxysertzuiolkjgvd"§$%&/()OPOLJHGFTE$%$%/(ZU'; // leave room
         console.log('user disconnected');
     });
     socket.on('chat message', function (msg) {
         console.log(socket.rooms[0] + ': message: ' + msg);
-        io.to('lol').emit('chat message', msg);
+            io.to(roomName).emit('chat message', msg);
     });
 });
 
